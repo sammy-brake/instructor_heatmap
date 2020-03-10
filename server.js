bbc2c86cf5b858a282ac26a22d6e5b0576a8a3da
sammy-brake <sammy.brake@gmail.com>
2020-03-10 17:27:33 -0500

    test api requests

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
-var AcuityScheduling = require('acuityscheduling');
-var express = require('express');
+const Acuity = require('acuityscheduling');
+const express = require('express');
+const config = require('./config.json')
 
+// create instance of acuity 
+let acuity = Acuity.basic(config);
+
+// create new express server
+let app = express();
+
+const PORT = 8000
+
+
+// test connection (do not run this...incredibly long output)
+// acuity.request('appointments', function (err, res, appointments) {
+//     if (err) return console.error(err);
+//     console.log(appointments);
+//   });
+
+acuity.request('appointment-types', function(err, res, appointments){
+    if (err) return console.error(err);
+    console.log(appointments)
+})
