8bf6486dba24e043c13f99d3e0fecda99c4df9cc
sammy-brake <sammy.brake@gmail.com>
2020-03-12 12:46:46 -0500

    create getAvailabilities function

--- server.js
+++ server.js
@@ -17,7 +17,51 @@ const PORT = 8000
 //     console.log(appointments);
 //   });
 
-acuity.request('appointment-types', function(err, res, appointments){
-    if (err) return console.error(err);
-    console.log(appointments)
-})
+
+// request for all appointment types
+// acuity.request('appointment-types', function(err, res, appointments){
+//     if (err) return console.error(err);
+//     console.log(appointments)
+// })
+
+// availability/times?appointmentTypeID=123&calendarID=123&date=2016-02-04"
+
+
+let date = '2020-03-18';
+let appointmentType = '3660432';
+let calendarId = '3580951';
+let calendarId2 = '3357862';
+let instructorId;
+// + '&addOnID=' + calendarId2
+instructors = [3580951,3357862,2367440]
+
+
+    
+function getAvailabilites(instructors){
+    for(let i = 0;i < instructors.length; i++){
+     
+        instructorId = instructors[i].toString()
+        console.log(i)
+        console.log("Instructor iD is......")
+        console.log(instructorId);
+
+    acuity.request('/availability/times?date=' + date + '&appointmentTypeID=' + appointmentType + '&calendarID=' + instructorId, function (err, res, times){
+        if (err) return console.error(err);
+        console.log(times);
+       
+    });
+    }
+}
+    
+    
+getAvailabilites(instructors)
+
+
+
+
+    // acuity.request('/availability/times?date=' + date + '&appointmentTypeID=' + appointmentType + '&calendarID=' + calendarId, function (err, res, times){
+    //     if (err) return console.error(err);
+    //     console.log(times);
+    //     // console.log(res)
+    // });
+
